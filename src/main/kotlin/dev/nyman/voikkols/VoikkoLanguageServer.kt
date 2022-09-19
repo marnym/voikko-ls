4f8f1e4c4015be229962b62bb3b2114e3a90f2f4
Markus Nyman <nymak@tuta.io>
2022-09-19 19:32:01 +0300

    implement basic services

---
+++ src/main/kotlin/dev/nyman/voikkols/VoikkoLanguageServer.kt
@@ -0,0 +1,48 @@
+package dev.nyman.voikkols
+
+import org.eclipse.lsp4j.InitializeParams
+import org.eclipse.lsp4j.InitializeResult
+import org.eclipse.lsp4j.ServerCapabilities
+import org.eclipse.lsp4j.TextDocumentSyncKind
+import org.eclipse.lsp4j.jsonrpc.messages.Either
+import org.eclipse.lsp4j.services.*
+import java.util.concurrent.CompletableFuture
+import kotlin.system.exitProcess
+
+class VoikkoLanguageServer : LanguageServer, LanguageClientAware {
+    private val textDocumentService = VoikkoTextDocumentService(this)
+    private val workspaceService = VoikkoWorkspaceService()
+    private val notebookDocumentService = VoikkoNotebookDocumentService()
+    var client: LanguageClient? = null
+    private var errorCode = 1
+
+    override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> {
+        val initializeResult = InitializeResult(ServerCapabilities()).apply {
+            capabilities.apply {
+                codeActionProvider = Either.forLeft(true)
+                textDocumentSync = Either.forLeft(TextDocumentSyncKind.Incremental)
+            }
+        }
+
+        return CompletableFuture.completedFuture(initializeResult)
+    }
+
+    override fun shutdown(): CompletableFuture<Any> {
+        errorCode = 0
+        return CompletableFuture.completedFuture(null)
+    }
+
+    override fun exit() {
+        exitProcess(errorCode)
+    }
+
+    override fun getTextDocumentService() = textDocumentService
+
+    override fun getWorkspaceService() = workspaceService
+
+    override fun getNotebookDocumentService() = notebookDocumentService
+
+    override fun connect(client: LanguageClient?) {
+        this.client = client
+    }
+}
---
+++ src/main/kotlin/dev/nyman/voikkols/VoikkoNotebookDocumentService.kt
@@ -0,0 +1,26 @@
+package dev.nyman.voikkols
+
+import org.eclipse.lsp4j.DidChangeNotebookDocumentParams
+import org.eclipse.lsp4j.DidCloseNotebookDocumentParams
+import org.eclipse.lsp4j.DidOpenNotebookDocumentParams
+import org.eclipse.lsp4j.DidSaveNotebookDocumentParams
+import org.eclipse.lsp4j.services.NotebookDocumentService
+
+class VoikkoNotebookDocumentService : NotebookDocumentService {
+    override fun didOpen(params: DidOpenNotebookDocumentParams?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun didChange(params: DidChangeNotebookDocumentParams?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun didSave(params: DidSaveNotebookDocumentParams?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun didClose(params: DidCloseNotebookDocumentParams?) {
+        TODO("Not yet implemented")
+    }
+
+}
